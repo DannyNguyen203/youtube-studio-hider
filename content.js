8163774bfb6f44ac0dd4fefcf70f5e8f1c4dab33
DannyNguyen203 <113634399+DannyNguyen203@users.noreply.github.com>
2023-02-23 00:00:24 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,16 @@
-const elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
-const elements2 = document.getElementsByClassName('metric-trend narrow metric-table-cell style-scope ytcd-channel-facts-item style-scope ytcd-channel-facts-item');
+hideIcons();
 
-// Hide the element
-for(let i = 0; i < elements.length; i++) {
-  elements[i].style.display = "none";
-}
-for(let i = 0; i < elements2.length; i++) {
-  elements2[i].style.cssText = 'display:none !important';
+window.onload = hideIcons();
+
+function hideIcons() {
+  const elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
+  const elements2 = document.getElementsByClassName('metric-trend narrow metric-table-cell style-scope ytcd-channel-facts-item style-scope ytcd-channel-facts-item');
+
+  // Hide the element
+  for(let i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none";
+  }
+  for(let i = 0; i < elements2.length; i++) {
+    elements2[i].style.cssText = 'display:none !important';
+  }
 }
