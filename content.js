7b19cfc4c6f9b2090c5897c7da7f4deb0f244c48
DannyNguyen203 <113634399+DannyNguyen203@users.noreply.github.com>
2023-02-22 21:14:19 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,6 @@
-const ytstudio = 'https://studio.youtube.com/channel';
+const elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
 
-
-// When the user clicks on the extension action
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(ytstudio)) {
-     function() {
-      // Manipulate elements here
-      var elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = 'none';;
-      }
-    }
+// Hide the element
+elements.forEach(function(element){
+  element.style.display = 'none';
 });
