c3cc0111dd9ffc4c54510a431788f1a75129c556
DannyNguyen203 <113634399+DannyNguyen203@users.noreply.github.com>
2023-02-22 16:20:59 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,53 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
-});
-
-
 const ytstudio = 'https://studio.youtube.com/channel';
 
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(ytstudio)) {
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-  
-
-    if (nextState === "ON") {
-      
-    chrome.tabs.executeScript(tab.id, 
-      function() {
+     function() {
       // Manipulate elements here
       var elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
       for (let i = 0; i < elements.length; i++) {
         elements[i].style.display = 'none';;
       }
-      
-    });
-
-      
-      
-    } else if (nextState === "OFF") {
-      
-    chrome.tabs.executeScript(tab.id,
-      function() {
-      var elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
-      // Manipulate elements here
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = 'inline';;
-      }
-      
-    });
     }
-  }
 });
