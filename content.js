95cffec2107550e811c0fcc292e3d4cde68d8f40
DannyNguyen203 <113634399+DannyNguyen203@users.noreply.github.com>
2023-02-22 22:21:41 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
 const elements = document.getElementsByClassName('table-icon typical-icon  style-scope yta-video-snapshot');
+const elements2 = document.getElementsByClassName('metric-trend narrow metric-table-cell style-scope ytcd-channel-facts-item style-scope ytcd-channel-facts-item');
 
 // Hide the element
 for(let i = 0; i < elements.length; i++) {
   elements[i].style.display = "none";
+}
+for(let i = 0; i < elements2.length; i++) {
+  elements2[i].style.display = "none";
 }
